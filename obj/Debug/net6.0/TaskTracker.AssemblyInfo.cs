f30d475de70f70a40b2acb54a485e1356b7ac666
Dilki Wijesooriya <dilkiwijesooriya@gmail.com>
2025-06-19 21:50:56 +0530

    clean unwanted files

--- obj/Debug/net6.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net6.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c645b98809b5d579b5b269621f50bdb549aad6b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db7efe12c705294d851eb39c83499642ba3fbe5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
